a2634a898e0647442ee272e5f8176a1321137818
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-29 11:46:21 -0400

    refactor update issue from webhook into service, refactor text and email notice into service

---
+++ app/services/issue_notifier.rb
@@ -0,0 +1,28 @@
+class IssueNotifier
+
+  attr_accessor :issue
+
+  def initialize(issue)
+    @issue = issue
+  end
+
+  def execute
+    send_email
+    send_text
+  end
+
+  def send_email
+    UserMailer.issue_update_email(issue.user, issue).deliver_now
+  end
+
+  def send_text
+    owner = issue.repository.user
+      if owner.phone_number
+        @client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN'])
+        @client.messages.create(
+          to: owner.phone_number, 
+          from: "+1 #{ENV['TWILIO_NUMBER']}",
+           body: "#{issue.title} has been updated. View it here: #{issue.url}")
+      end
+  end
+end
